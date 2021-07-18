b3f378d3e84b415a897372b7bb0092e74a319344
Ankur Shrivastava <35427537+aber6@users.noreply.github.com>
2021-07-18 23:29:38 +0530

    Create 1328A.cpp

---
+++ 1328A.cpp
@@ -0,0 +1,19 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+    int main()
+    {
+        int t;
+        cin>>t;
+        for(int i=0;i<t;i++)
+        {
+            int a,b;
+            cin>>a>>b;
+            int x=a%b;
+            if(a%b==0)
+            {
+                cout<<0<<endl;
+            }
+            else cout<<b-x<<endl;
+            
+        }
+    }
