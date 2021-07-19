82e04496bec95ed27eb0bc729683a9b4ef090682
Ankur Shrivastava <35427537+aber6@users.noreply.github.com>
2021-07-19 13:35:05 +0530

    Create 624A.cpp

---
+++ 624A.cpp
@@ -0,0 +1,9 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+    int main()
+    {
+        double d,l,v1,v2;
+        cin>>d>>l>>v1>>v2;
+        double x=(l-d)/(v1+v2);
+        cout<<setprecision(20)<<x<<endl;
+    }
